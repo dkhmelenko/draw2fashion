bb2f959ee232c5e89ac77f70d0e36660b3165bba
Dmytro Khmelenko <d.khmelenko@gmail.com>
2017-09-16 04:28:01 +0200

    Fix constraint layout

--- app/src/main/java/org/hackzurich2017/draw2fashion/MainActivity.kt
+++ app/src/main/java/org/hackzurich2017/draw2fashion/MainActivity.kt
@@ -1,7 +1,8 @@
 package org.hackzurich2017.draw2fashion
 
-import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.support.v7.app.AppCompatActivity
+import kotlinx.android.synthetic.main.activity_main.*
 import org.hackzurich2017.draw2fashion.draw2fashion.R
 
 class MainActivity : AppCompatActivity() {
@@ -9,5 +10,15 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        saveButton.setOnClickListener { saveImage() }
+    }
+
+    private fun saveImage() {
+        var bitmap = drawingPad.signatureBitmap
+
+        // TODO Implement sending bitmap for image recognition
+
+        drawingPad.clear()
     }
 }
